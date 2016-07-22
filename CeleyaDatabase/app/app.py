a92e15ff14cb11d1a11b3deebe40ae07809dbbf8
adam alloy <aralloy@gmail.com>
2016-07-22 20:20:21 +0000

    changes to app

--- CeleyaDatabase/app/app.py
+++ CeleyaDatabase/app/app.py
@@ -6,7 +6,6 @@ import requests
 from flask.ext.sqlalchemy import SQLAlchemy
 import json
 import csv
-from flask.ext.pymongo import PyMongo
 from db import db
 # import logging why do I need logging
 
@@ -54,9 +53,9 @@ This converts the comma separated values input file to a more usable JSON file.
 Should I include the path as an input
 """
 def convercsv():
-	csvfile = open('/adamalloy/downloads/CeleyaData.csv', 'r')
+	csvfile = open('~/CeleyaData.csv', 'r')
 	reader = csv.DictReader(csvfile, fieldnames =  ("Team Number", "Station Number", "Date and Time of Data", "Coordinates of Site", 
-"Prepended text", "Station references", "Fault Name", "Data Type", "Strike of Fault", "Angle Between Surface and Slip or Dip", "Throw of Fault", "Slip of Fault", "Hand Written Notes", "Image 1 Link", "Image 1 Subject", "Image Description and Key Words", "Image 2 Link", "Image 2 Subject", "Image 2 Descrition and Key Words", "Image 3 Link","Image 3 Subject", "Image 3 Description and Key Words", "Track Map Image", "Track Map URL", "More Comments")
+"Prepended text", "Station references", "Fault Name", "Data Type", "Strike of Fault", "Angle Between Surface and Slip or Dip", "Throw of Fault", "Slip of Fault", "Hand Written Notes", "Image 1 Link", "Image 1 Subject", "Image Description and Key Words", "Image 2 Link", "Image 2 Subject", "Image 2 Descrition and Key Words", "Image 3 Link","Image 3 Subject", "Image 3 Description and Key Words", "Track Map Image", "Track Map URL", "More Comments"))
 	out = json.dumps([ row for row in reader])
 	print out
 	return out
@@ -68,35 +67,35 @@ def convercsv():
 
 class SubsidencePoint(db.Model):
 
-__tablename__ = 'SubsidencePoint'
+	__tablename__ = 'SubsidencePoint'
 
-# Dimensions
-id = db.Column(db.Integer, primary_key=True)
-team_number = db.Column(db.String(256))
-station_number = db.Column(db.String(256))
-data_time = db.Column(db.String(256))
-coordinates = db.Column(db.String(256))
-prepended_text = db.Column(db.String(256))
-station_references = db.Column(db.String(256))
-fault = db.Column(db.String(256))
-data_type = db.Column(db.String(256))
-strike = db.Column(db.Integer) #this may need to be float or string
-angle = db.Column(db.Integer) #same
-throw = db.Column(db.Integer) #same
-slip = db.Column(db.Integer) #same
-notes = db.Column(db.String(256))
-image_1_link = db.Column(db.String(256)) 
-image_1_subject = db.Column(db.String(256))
-image_1_description = db.Column(db.String(256))
-image_2_link = db.Column(db.String(256)) 
-image_2_subject = db.Column(db.String(256))
-image_2_description = db.Column(db.String(256))
-image_3_link = db.Column(db.String(256)) 
-image_3_subject = db.Column(db.String(256))
-image_3_description = db.Column(db.String(256))
+	# Dimensions
+	id = db.Column(db.Integer, primary_key=True)
+	team_number = db.Column(db.String(256))
+	station_number = db.Column(db.String(256))
+	data_time = db.Column(db.String(256))
+	coordinates = db.Column(db.String(256))
+	prepended_text = db.Column(db.String(256))
+	station_references = db.Column(db.String(256))
+	fault = db.Column(db.String(256))
+	data_type = db.Column(db.String(256))
+	strike = db.Column(db.Integer) #this may need to be float or string
+	angle = db.Column(db.Integer) #same
+	throw = db.Column(db.Integer) #same
+	slip = db.Column(db.Integer) #same
+	notes = db.Column(db.String(256))
+	image_1_link = db.Column(db.String(256)) 
+	image_1_subject = db.Column(db.String(256))
+	image_1_description = db.Column(db.String(256))
+	image_2_link = db.Column(db.String(256)) 
+	image_2_subject = db.Column(db.String(256))
+	image_2_description = db.Column(db.String(256))
+	image_3_link = db.Column(db.String(256)) 
+	image_3_subject = db.Column(db.String(256))
+	image_3_description = db.Column(db.String(256))
 
 	def serialize(self):
-	return dict(id = self.id, team_number = self.team_number, station_number = self.station_number, data_time = self.data_time, coordinates = self.coordinates, prepended_text = self.prepended_text, station_references = self.station_references , fault = self.fault, data_type = self.data_type, strike = self.strike, angle = self.angle, throw = self.throw, slip = self.slip, notes = self.notes, image_1_link = self. image_1_link, image_1_subject = self.image_1_subject, image_1_description = self.image_1_description, image_2_link = self.image_2_link, image_2_subject = self.image_2_subject, image_2_description = self.image_2_description,image_3_link = self.image_3_link, image_3_subject = self.image_3_subject, image_3_description = self.image_3_description)
+		return dict(id = self.id,team_number = self.team_number,station_number = self.station_number,data_time = self.data_time,coordinates = self.coordinates,prepended_text = self.prepended_text,station_references = self.station_references,fault = self.fault,data_type = self.data_type,strike = self.strike,angle = self.angle,throw = self.throw,slip = self.slip,notes = self.notes,image_1_link = self.image_1_link,image_1_subject = self.image_1_subject,image_1_description = self.image_1_description,image_2_link = self.image_2_link,image_2_subject = self.image_2_subject,image_2_description = self.image_2_description,image_3_link = self.image_3_link,image_3_subject = self.image_3_subject,image_3_description = self.image_3_description)
 
 #-------------
 # initialize tables
@@ -105,10 +104,10 @@ image_3_description = db.Column(db.String(256))
 
 def init_subsidence_point(out):
 	for data in out:
-	s = SubsidencePoint(team_number = data["team_number"], station_number ["station_number"], data_time = data["data_time"], coordinates = data["coordinates"], prepended_text = data["prepended_text"], station_references = data["station_references"] , fault =data["fault"], data_type = data["data_type"], strike = data["strike"], angle = data["angle"], throw = data["throw"], slip = data["slip"], notes = data["notes"], image_1_link = data["image_1_link"], image_1_subject = data["image_1_subject"], image_1_description = data["image_1_description"], image_2_link = data["image_2_link"], image_2_subject = data["image_2_subject"], image_2_description = data["image_2_description"],image_3_link = data["image_3_link"], image_3_subject = data["image_3_subject"], image_3_description = data["image_3_description"])
+		s = SubsidencePoint(team_number = data["team_number"], station_number = data["station_number"], data_time = data["data_time"], coordinates = data["coordinates"], prepended_text = data["prepended_text"], station_references = data["station_references"] , fault =data["fault"], data_type = data["data_type"], strike = data["strike"], angle = data["angle"], throw = data["throw"], slip = data["slip"], notes = data["notes"], image_1_link = data["image_1_link"], image_1_subject = data["image_1_subject"], image_1_description = data["image_1_description"], image_2_link = data["image_2_link"], image_2_subject = data["image_2_subject"], image_2_description = data["image_2_description"],image_3_link = data["image_3_link"], image_3_subject = data["image_3_subject"], image_3_description = data["image_3_description"])
 
 @manager.command
-def init_database
+def init_db():
 	db.drop_all()
 	db.create_all()
 	logger.debug("initializing subsidence points")
